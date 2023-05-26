a97870c9e06a77ace44a1708ddd69f559f893bdc
ClecioSousa00 <cleciosouza015@gmail.com>
2023-05-26 12:53:12 -0300

    fix: use client

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,10 +1,12 @@
-import bgVideo from '../../assets/video/Background_video.mp4'
-
+'use client'
 export const Background = () => {
   return (
     <div className="fixed inset-0 z-[-1]  h-screen w-full after:fixed after:inset-0  after:h-screen after:w-screen  after:bg-shadow after:content-['']">
       <video loop autoPlay muted className="h-full w-full object-cover">
-        <source src={bgVideo} type="video/mp4" />
+        <source
+          src={require('../../assets/video/Background_video.mp4')}
+          type="video/mp4"
+        />
       </video>
     </div>
   )
